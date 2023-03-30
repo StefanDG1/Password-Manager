da0f1836e6630c9b5569e07e597c2715dfa92e91
StefanDG1 <danistefangheorghiu@gmail.com>
2023-03-30 14:00:34 +0200

    added tray, close hides window instead, new icon

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const { BrowserWindow, app, ipcMain, dialog } = require('electron');
+const { BrowserWindow, app, ipcMain, dialog, Tray, Menu } = require('electron');
 const path = require('path');
-const fs = require("fs");
+const fs = require('fs');
 
-
+let mainWindow;
 const createWindow = () => {
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         width: 1000,
         height: 700,
+        title: 'MiniPass',
+        icon: path.join(__dirname, '/images/icon.png'),
         webPreferences: {
             contextIsolation: true,
             sandbox: false,
@@ -16,11 +18,63 @@ const createWindow = () => {
 
     mainWindow.loadFile(path.join(__dirname, "index.html"));
     //win.removeMenu();
+
+
+    // hide window on close
+    mainWindow.on('close', (event) => {
+        if (!isQuiting) {
+            event.preventDefault();
+            mainWindow.hide();
+            event.returnValue = false;
+        }
+    });
 }
+
+
+// hide window on minimize or close
+
+let isQuiting;
+let tray;
+
+app.on('before-quit', () => {
+    isQuiting = true;
+});
+
 
 app.whenReady().then(() => {
     createWindow();
+
+    // system tray
+    tray = new Tray('./images/icon.png');
+    const contextMenu = Menu.buildFromTemplate([{
+            label: 'Show App',
+            click: function () {
+                mainWindow.show();
+            }
+        },
+        {
+            label: 'Quit',
+            click: function () {
+                isQuiting = true;
+                app.quit();
+            }
+        }
+    ]);
+
+    tray.setToolTip('MiniPass');
+    tray.setContextMenu(contextMenu);
+    tray.setIgnoreDoubleClickEvents(true);
+    tray.on('click', () => {
+        if (mainWindow.isVisible()) {
+            mainWindow.hide()
+        } else {
+            mainWindow.show()
+        }
+    })
 });
+
+
+
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
@@ -30,11 +84,15 @@ app.on('window-all-closed', () => {
 ipcMain.handle('dialog:openFile', handleFileOpen)
 
 async function handleFileOpen() {
-    const { canceled, filePaths } = await dialog.showOpenDialog({ 
+    const {
+        canceled,
+        filePaths
+    } = await dialog.showOpenDialog({
         properties: ['openFile'],
-        filters: [
-            { name: 'Images', extensions: ['jpg', 'png', 'gif', 'svg'] },
-          ]
+        filters: [{
+            name: 'Images',
+            extensions: ['jpg', 'png', 'gif', 'svg']
+        }, ]
     });
     if (canceled) {
         return
